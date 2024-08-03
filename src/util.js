ee299ea6c188604c3176bbad2893fece8f6b042b
StockerMC <44980366+StockerMC@users.noreply.github.com>
2024-08-03 14:29:13 -0400

    Blob to base64 util

---
+++ src/util.js
@@ -0,0 +1,8 @@
+function blobToBase64(blob) {
+    return new Promise((resolve, _) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result);
+      reader.readAsDataURL(blob);
+    });
+  }
+  
